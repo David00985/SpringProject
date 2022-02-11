f135e797cf9d4e60c1c4029bc3b9b9dc2e2b5688
박종윤 <qkrwhddbs97@naver.com>
2022-02-11 18:30:32 +0900

    박종윤

--- SprintProject/src/main/java/com/controller/OrderController.java
+++ SprintProject/src/main/java/com/controller/OrderController.java
@@ -1,10 +1,17 @@
 package com.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.dto.MemberDTO;
 import com.dto.OrderDTO;
 import com.service.OrderService;
 
@@ -15,9 +22,13 @@ public class OrderController {
 	OrderService service;
 	
 	@RequestMapping(value = "/loginCheck/orderChart")
-	public String orderChart(@RequestParam String mid, OrderDTO dto){
+	public String orderChart(HttpSession session){
 		System.out.println("주문조회 기능");
-		
+		MemberDTO dto = (MemberDTO) session.getAttribute("login_member");
+		String mid = dto.getMid();
+		List<OrderDTO> list = service.orderChart(mid);
+		System.out.println(list);
+		session.setAttribute("orderChart", list);
 		return "orderChart";
 	}
 	
